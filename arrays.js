e2d7c34d473a5a7b33c036a6718c771bd1e0f06e
jvpink200 <jvillarr20@gmail.com>
2018-08-25 03:39:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,2 @@
-var chocolateBars = ["snickers","hundred grand", "kitkat","skittles"];
-
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
-}
-
-destructivelyAddElementToBeginningOfArray(array,element){
-array.unshift(element);
-return array;
-}
+var chocolateBars = new Array();
+chocolateBars = ["snickers", "hundred grand","kitkat","skittles"];
